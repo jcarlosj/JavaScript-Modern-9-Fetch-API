9f33605f23c694f362dceebb4a7b0026a9c4dacc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-03 10:47:18 -0500

    Refactoriza código: Cambia 'functions' por 'Arrow Functions' (REST API con Fetch API)

--- js/app.js
+++ js/app.js
@@ -12,86 +12,59 @@ document .getElementById( 'apiBTN' ) .addEventListener( 'click', cargarREST );
 // Function carga datos del archivo datos.txt
 function cargarTXT() {
     // FetchAPI usando Promises
-    fetch( 'datos.txt' )                    // Se conecta a los datos
-       .then( function( response ) {        // Recibe la respuesta
-        console .log( 'Respuesta', response );
-
-        return response .text();            // Promise: 
-                                            //   Indica el método en el que se recibirán los datos, se puede revisar 
-                                            //   el 'proto' para ver los métodos disponibles
-
-    }) .then( function( data ) {            // Recibe los datos en el formato del método indicado anteriormente
-        console .log( data );
-        document .getElementById( 'resultado' ) .innerHTML = data;      // Coloca los datos obtenidos en el DOM
-    }) .catch( function( error ) {          // Captura los ERRORES
-        console .log( 'ERROR', error );
-    });
+    fetch( 'datos.txt' )                                                            // Se conecta a los datos
+        .then( response => response .text() )                                       // Recibe la respuesta     
+        .then( data => document .getElementById( 'resultado' ) .innerHTML = data )  // Coloca los datos obtenidos en el DOM
+        .catch( error => console .log( 'ERROR', error ) );                          // Captura los ERRORES
 }
 
 // Function carga datos del archivo datos.txt
 function cargarJSON() {
     // FetchAPI usando Promises
-    fetch( 'empleados.json' )               // Se conecta a los datos
-        .then( function( response ) {       // Recibe la respuesta
-            console .log( 'Respuesta', response );
+    fetch( 'empleados.json' )                                                       // Se conecta a los datos
+        .then( response => response .json() )                                       // Recibe la respuesta
+        .then( data => {                                                            // Coloca los datos obtenidos en el DOM
+            let html = '<ul>';                                                      // Crea un "Template"
 
-            return response .json();        // Promise: 
-                                            //   Indica el método en el que se recibirán los datos, se puede revisar 
-                                            //   el 'proto' para ver los métodos disponibles
-    }) .then( function( data ) {
-        let html = '<ul>';                  // Crea un "Template"
+            console .log( data );
 
-        console .log( data );
+            // Recorre el 'Array' de objetos empleado
+            data .forEach( empleado => {
+                // Agrega los datos al Template
+                html += `
+                    <li>${ empleado .nombre } - <b> ${ empleado .puesto } </b></li>
+                `;
+            });
 
-        // Recorre el 'Array' de objetos empleado
-        data .forEach( function( empleado ) {
-            // Agrega los datos al Template
-            html += `
-                <li>${ empleado .nombre } - <b> ${ empleado .puesto } </b></li>
-            `;
-        });
+            html += '</ul>';
+            document .getElementById( 'resultado' ) .innerHTML = html;              // Coloca los datos obtenidos en el DOM
 
-        html += '</ul>';
-        document .getElementById( 'resultado' ) .innerHTML = html;  // Coloca los datos obtenidos en el DOM
-
-    }) .catch( function( error ) {          // Captura los ERRORES
-        console .log( 'ERROR', error );
-    });
+        }) .catch( error => console .log( 'ERROR', error ) );                       // Captura los ERRORES
 }   
 
 // Function carga datos del archivo datos.txt
 function cargarREST() {
     // FetchAPI usando Promises
-    fetch( 'http://picsum.photos/list' )    // Se conecta a los datos
-       .then( function( response ) {        // Recibe la respuesta
-        console .log( 'Respuesta', response );
+    fetch( 'http://picsum.photos/list' )                                            // Se conecta a los datos
+       .then( response => response .json() )                                        // Recibe la respuesta
+       .then( imagenes => {                                                         // Recibe los datos en el formato del método indicado anteriormente
+            let html = '<ul>';                                                      // Crea un "Template"
 
-        return response .json();            // Promise: 
-                                            //   Indica el método en el que se recibirán los datos, se puede revisar 
-                                            //   el 'proto' para ver los métodos disponibles
+            console .log( imagenes );
 
-    }) .then( function( imagenes ) {        // Recibe los datos en el formato del método indicado anteriormente
-        let html = '<ul>';                  // Crea un "Template"
+            // Recorre el 'Array' de objetos de imagenes
+            imagenes .forEach( imagen => {
+                // Agrega los datos al Template
+                html += `
+                <li style="list-style-type: none;">
+                        <b>Autor:</b> ${ imagen .author } <br />
+                        <a target="_blank" href="${ imagen .post_url }">Ver Imagen</a> 
+                </li>
+                `;
+            });
 
-        console .log( imagenes );
+            html += '</ul>';
+            document .getElementById( 'resultado' ) .innerHTML = html;              // Coloca los datos obtenidos en el DOM
 
-        // Recorre el 'Array' de objetos de imagenes
-        imagenes .forEach( function( imagen ) {
-            // Agrega los datos al Template
-            html += `
-               <li style="list-style-type: none;">
-                    <b>Autor:</b> ${ imagen .author } <br />
-                    <a target="_blank" href="${ imagen .post_url }">Ver Imagen</a> 
-               </li>
-            `;
-        });
-
-        html += '</ul>';
-        document .getElementById( 'resultado' ) .innerHTML = html;  // Coloca los datos obtenidos en el DOM
-
-
-
-    }) .catch( function( error ) {          // Captura los ERRORES
-        console .log( 'ERROR', error );
-    });
+        }) .catch( error => console .log( 'ERROR', error ) );                       // Captura los ERRORES
 }
